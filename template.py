e0ee3530d9e355c2c79f2913325878357b8c0113
rj1 <rj1@riseup.net>
2022-11-30 13:49:00 -0600

    added a template for solutions so we can start quicker

---
+++ template.py
@@ -0,0 +1,7 @@
+import os
+
+base_dir = os.path.realpath(os.path.dirname(__file__))
+with open(base_dir + "/input.txt", "r") as file:
+    input = file.read().rstrip()
+
+# lines = input.split("\n")
